eabcdbda63090c22b191d22fc6ef3473f8d9f0de
David Mills <david.mills2@outlook.com>
2021-02-27 15:16:12 -0800

    Add project files.

---
+++ Array8G/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Array8G
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+
+            int[] myArray = new int[64];
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                myArray[i] = i;
+
+            }
+
+            foreach (var i in myArray)
+            {
+                Console.WriteLine(myArray[i]);
+            }
+
+        }
+      
+    }
+}
